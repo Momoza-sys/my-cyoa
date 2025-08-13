4f34a3c31a8f799eca54e55a5dbab9cafd2485d3
Momoza-sys <nksmanga2020@gmail.com>
2025-08-13 04:07:51 -0700

    vsCode_sucks_wont_push

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+});
 
-export default nextConfig;
+export default withMDX({
+  plugins: [require('@tailwindcss/typography')],
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
